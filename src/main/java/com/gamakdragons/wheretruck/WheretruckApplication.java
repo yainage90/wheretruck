ef3884b51c1d4990e5c5de437fa50258ba431124
yaincoding <yaincoding@gmail.com>
2021-05-23 17:43:15 +0900

    first commit

---
+++ src/main/java/com/gamakdragons/wheretruck/WheretruckApplication.java
@@ -0,0 +1,13 @@
+package com.gamakdragons.wheretruck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheretruckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheretruckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamakdragons/wheretruck/WheretruckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamakdragons.wheretruck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheretruckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
