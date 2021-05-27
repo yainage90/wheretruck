b5d76629bb436be9f4e593bd6cb1bd857614f86a
yaincoding <yaincoding@gmail.com>
2021-05-28 00:35:33 +0900

    scripts for make&delete indices

---
+++ scripts/delete_indices.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+curl -XDELETE 'http://ec2-13-209-181-246.ap-northeast-2.compute.amazonaws.com:9200/permission_region?pretty'
+curl -XDELETE 'http://ec2-13-209-181-246.ap-northeast-2.compute.amazonaws.com:9200/truck?pretty'
+curl -XDELETE 'http://ec2-13-209-181-246.ap-northeast-2.compute.amazonaws.com:9200/menu?pretty'
+curl -XDELETE 'http://ec2-13-209-181-246.ap-northeast-2.compute.amazonaws.com:9200/review?pretty'
+curl -XDELETE 'http://ec2-13-209-181-246.ap-northeast-2.compute.amazonaws.com:9200/favorite?pretty'
+
