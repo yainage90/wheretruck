c06e0a888c98111f1942e5f94741d0e3de2384c2
yaincoding <yaincoding@gmail.com>
2021-05-30 21:46:48 +0900

    isOpened field of truck index is changed to opened

--- scripts/make_indices.sh
+++ scripts/make_indices.sh
@@ -93,7 +93,7 @@ curl -XPUT 'http://ec2-13-209-181-246.ap-northeast-2.compute.amazonaws.com:9200/
       "description": {
         "type": "keyword"
       },
-      "isOpened": {
+      "opened": {
         "type": "boolean"
       }
     }
