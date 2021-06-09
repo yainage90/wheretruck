0430df0203b4dbf606ccdf0ef7981a69b8d8236b
yaincoding <yaincoding@gmail.com>
2021-06-10 01:28:33 +0900

    @Primary annotation added because of conflict of auto bean generation with property file resources

--- src/main/java/com/gamakdragons/wheretruck/config/S3Config.java
+++ src/main/java/com/gamakdragons/wheretruck/config/S3Config.java
@@ -1,5 +1,6 @@
 package com.gamakdragons.wheretruck.config;
 
+import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
@@ -10,6 +11,7 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class S3Config {
@@ -20,11 +22,19 @@ public class S3Config {
     @Value("${cloud.aws.credentials.secretKey}")
     private String secretKey;
 
+    @Primary
     @Bean
     public AmazonS3 initS3Client() {
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        ClientConfiguration config = new ClientConfiguration();
+        config.setConnectionTimeout(20000);
+        config.setSocketTimeout(20000);
+        config.setMaxErrorRetry(3);
+        config.setMaxConnections(30);
+        
         return AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withClientConfiguration(config)
                 .withRegion(Regions.AP_NORTHEAST_2)
                 .build();
     }
