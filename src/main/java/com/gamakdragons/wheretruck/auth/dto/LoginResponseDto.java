c8f1038a99d03f44f9d609c651b08db0ade5267a
yaincoding <yaincoding@gmail.com>
2021-06-12 02:51:14 +0900

    login/logout response dto fields changed

--- src/main/java/com/gamakdragons/wheretruck/auth/dto/LoginResponseDto.java
+++ src/main/java/com/gamakdragons/wheretruck/auth/dto/LoginResponseDto.java
@@ -3,10 +3,12 @@ package com.gamakdragons.wheretruck.auth.dto;
 import com.gamakdragons.wheretruck.domain.user.entity.User;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.ToString;
 
 @Builder
 @ToString
+@Getter
 public class LoginResponseDto {
 	
 	User user;
--- src/main/java/com/gamakdragons/wheretruck/auth/dto/LogoutResponseDto.java
+++ src/main/java/com/gamakdragons/wheretruck/auth/dto/LogoutResponseDto.java
@@ -1,8 +1,10 @@
 package com.gamakdragons.wheretruck.auth.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.ToString;
 
+@Getter
 @Builder
 @ToString
 public class LogoutResponseDto {
