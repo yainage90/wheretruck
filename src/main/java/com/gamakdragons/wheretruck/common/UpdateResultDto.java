ad5bc2252cdfbe35ea5a90cdb602e602669417ae
yaincoding <yaincoding@gmail.com>
2021-06-12 00:02:37 +0900

    @ToString lombok annotation added for debug

--- src/main/java/com/gamakdragons/wheretruck/common/UpdateResultDto.java
+++ src/main/java/com/gamakdragons/wheretruck/common/UpdateResultDto.java
@@ -2,9 +2,11 @@ package com.gamakdragons.wheretruck.common;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @Builder
+@ToString
 public class UpdateResultDto {
 
     private String result;
