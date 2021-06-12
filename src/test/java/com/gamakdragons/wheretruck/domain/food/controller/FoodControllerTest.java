b12e36c46e404d0bbbf8c50ab2591a8536e34a56
yaincoding <yaincoding@gmail.com>
2021-06-13 04:00:03 +0900

    FoodController MockMvc Test created

--- src/test/java/com/gamakdragons/wheretruck/domain/food/controller/FoodControllerTest.java
+++ src/test/java/com/gamakdragons/wheretruck/domain/food/controller/FoodControllerTest.java
@@ -1,26 +1,151 @@
 package com.gamakdragons.wheretruck.domain.food.controller;
 
-import org.junit.jupiter.api.Test;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gamakdragons.wheretruck.common.UpdateResultDto;
+import com.gamakdragons.wheretruck.domain.food.dto.FoodSaveRequestDto;
+import com.gamakdragons.wheretruck.domain.food.service.FoodService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@WebMvcTest(FoodController.class)
 public class FoodControllerTest {
 
-	@Test
-	void testDelete() {
+	@Autowired
+	private MockMvc mockMvc;
 
+	@MockBean
+	private FoodService foodService;
+
+	private ObjectMapper objectMapper;
+
+	@BeforeEach
+	public void setup() {
+		objectMapper = new ObjectMapper();
 	}
 
 	@Test
-	void testSave() {
+	void testDelete() throws Exception {
+
+		UpdateResultDto result = UpdateResultDto.builder().result("UPDATED").build();
+
+		String truckId = UUID.randomUUID().toString();
+		String foodId = UUID.randomUUID().toString();
+
+		given(foodService.deleteFood(truckId, foodId)).willReturn(result);
+
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.delete("/api/food/" + truckId + "/" + foodId);
+
+		mockMvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/json"))
+				.andExpect(content().string(objectMapper.writeValueAsString(result)));
+	}
+
+	@Test
+	void testSave() throws Exception {
 		
+		String truckId = UUID.randomUUID().toString();
+		String foodId = UUID.randomUUID().toString();
+
+		byte[] imageBinary = new byte[128];
+        new Random().nextBytes(imageBinary);
+        MockMultipartFile image = new MockMultipartFile("image", "foodImage.png", MediaType.MULTIPART_FORM_DATA_VALUE, imageBinary);
+
+		FoodSaveRequestDto foodSaveRequestDto = new FoodSaveRequestDto();
+		foodSaveRequestDto.setName("팥빵");
+		foodSaveRequestDto.setCost(1000);
+		foodSaveRequestDto.setDescription("맛있어요");
+		foodSaveRequestDto.setImage(image);
+
+		UpdateResultDto result = UpdateResultDto.builder().result("UPDATED").id(foodId).build();
+
+		given(foodService.saveFood(truckId, foodSaveRequestDto)).willReturn(result);
+
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.multipart("/api/food/" + truckId)
+																	.file(image)
+																	.contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+																	.param("name", foodSaveRequestDto.getName())
+																	.param("cost", String.valueOf(foodSaveRequestDto.getCost()))
+																	.param("description", foodSaveRequestDto.getDescription());
+
+		mockMvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/json"))
+				.andExpect(content().string(objectMapper.writeValueAsString(result)));
 	}
 
 	@Test
-	void testSort() {
+	void testSort() throws Exception {
 
+		String truckId = UUID.randomUUID().toString();
+		List<String> ids = new ArrayList<>();
+		for(int i = 0; i < 5; i++) {
+			ids.add(UUID.randomUUID().toString());
+		}
+
+		UpdateResultDto result = UpdateResultDto.builder().result("UPDATED").build();
+
+		given(foodService.sortFoods(truckId, ids)).willReturn(result);
+
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.put("/api/food/sort/" + truckId + "/" + String.join(",", ids));
+
+		mockMvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/json"))
+				.andExpect(content().string(objectMapper.writeValueAsString(result)));
 	}
 
-	@Test
-	void testUpdate() {
+	/*@Test
+	void testUpdate() throws Exception {
 
-	}
+		String truckId = UUID.randomUUID().toString();
+		String foodId = UUID.randomUUID().toString();
+
+		byte[] imageBinary = new byte[128];
+        new Random().nextBytes(imageBinary);
+        MockMultipartFile image = new MockMultipartFile("image", "foodImage.png", MediaType.MULTIPART_FORM_DATA_VALUE, imageBinary);
+
+		FoodUpdateRequestDto foodUpdateRequestDto = new FoodUpdateRequestDto();
+		foodUpdateRequestDto.setId(foodId);
+		foodUpdateRequestDto.setName("팥빵");
+		foodUpdateRequestDto.setCost(1000);
+		foodUpdateRequestDto.setDescription("맛있어요");
+		foodUpdateRequestDto.setImage(image);
+
+		UpdateResultDto result = UpdateResultDto.builder().result("UPDATED").id(foodId).build();
+
+		given(foodService.updateFood(truckId, foodUpdateRequestDto)).willReturn(result);
+
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.multipart("/api/food/" + truckId)
+																	.
+																	.file(image)
+																	.contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+																	.param("id", foodUpdateRequestDto.getId())
+																	.param("name", foodUpdateRequestDto.getName())
+																	.param("cost", String.valueOf(foodUpdateRequestDto.getCost()))
+																	.param("description", foodUpdateRequestDto.getDescription());
+
+		mockMvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/json"))
+				.andExpect(content().string(objectMapper.writeValueAsString(result)));
+	}*/
 }
