8991cb6997ca822e95120ebd7a9aee5e2b6ddfcc
yaincoding <yaincoding@gmail.com>
2021-06-14 01:57:49 +0900

    S3ServiceException class created for aws s3 related exceptions

---
+++ src/main/java/com/gamakdragons/wheretruck/cloud/aws/exception/S3ServiceException.java
@@ -0,0 +1,8 @@
+package com.gamakdragons.wheretruck.cloud.aws.exception;
+
+public class S3ServiceException extends RuntimeException {
+	
+	public S3ServiceException(String msg) {
+		super(msg);
+	}
+}
