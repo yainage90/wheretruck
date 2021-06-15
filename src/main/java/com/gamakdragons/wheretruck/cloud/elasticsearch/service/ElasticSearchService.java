bfcb04edfe6da0a3be32c085b04dfae38b845800
yaincoding <yaincoding@gmail.com>
2021-06-16 00:40:28 +0900

    elasticsearch service class removed

---
+++ src/main/java/com/gamakdragons/wheretruck/cloud/elasticsearch/service/ElasticSearchService.java
@@ -0,0 +1,32 @@
+package com.gamakdragons.wheretruck.cloud.elasticsearch.service;
+
+import java.io.IOException;
+
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.get.MultiGetRequest;
+import org.elasticsearch.action.get.MultiGetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.index.reindex.BulkByScrollResponse;
+import org.elasticsearch.index.reindex.DeleteByQueryRequest;
+
+public interface ElasticSearchService {
+
+    GetResponse get(GetRequest request, RequestOptions options) throws IOException;
+    MultiGetResponse multiGet(MultiGetRequest request, RequestOptions options) throws IOException;
+
+    SearchResponse search(SearchRequest searchRequest, RequestOptions options) throws IOException;
+
+    IndexResponse index(IndexRequest request, RequestOptions options) throws IOException; 
+    UpdateResponse update(UpdateRequest request, RequestOptions options) throws IOException;
+    DeleteResponse delete(DeleteRequest request, RequestOptions options) throws IOException;
+    BulkByScrollResponse deleteByQuery(DeleteByQueryRequest request, RequestOptions options) throws IOException;
+}
